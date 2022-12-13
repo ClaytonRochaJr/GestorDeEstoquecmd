41bb07d388f04559c0d1c41d8582a020a9dca928
Clayton Rocha de Barros Junior <claytonrocha179@gmail.com>
2022-12-13 18:53:59 -0300

    gestor de estoque cmd

---
+++ GestorDeEstoqueCMD/Ebook.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeEstoqueCMD
+{
+    [System.Serializable]
+    class Ebook : Produto, IEstoque
+    {
+        public string autor;
+        private int vendas;
+
+        public Ebook(string nome, float preco, string autor)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.autor = autor;
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine("Não é possivel dar entrada no estoque de um Ebook, pois é um produto digital!");
+            Console.ReadLine();
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Adicionar vendas no E-book {nome}");
+            Console.WriteLine("Digite a quantidade de vendas que você quer dar entrada: ");
+            int entrada = int.Parse(Console.ReadLine());
+            vendas += entrada;
+            Console.WriteLine("Venda registrada!");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Vendas: {vendas}");
+            Console.WriteLine($"Autor: {autor}");
+            Console.WriteLine("=================================");
+        }
+    }
+}
---
+++ GestorDeEstoqueCMD/ProdutoFisico.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeEstoqueCMD
+{
+    [System.Serializable]
+     class ProdutoFisico : Produto, IEstoque
+    {
+        public float frete;
+        private int estoque;
+        public ProdutoFisico(string nome,  float frete,  float preco)
+        {
+            this.nome = nome;
+            this.frete = frete;
+            this.preco = preco;
+        }
+
+        public void AdicionarEntrada()
+        {
+            Console.WriteLine($"Adicionar entrada no estoque do preduto {nome}");
+            Console.WriteLine("Digite a quantidade de produtos que você quer dar entrada: ");
+            int entrada = int.Parse(Console.ReadLine());
+            estoque += entrada;
+            Console.WriteLine("Entrada registrada");
+            Console.ReadLine();
+        }
+
+        public void AdicionarSaida()
+        {
+            Console.WriteLine($"Adicionar saída no estoque do preduto {nome}");
+            Console.WriteLine("Digite a quantidade de produtos que você quer dar baixa: ");
+            int entrada = int.Parse(Console.ReadLine());
+            estoque -= entrada;
+            Console.WriteLine("saída registrada");
+            Console.ReadLine();
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Preço: {preco}");
+            Console.WriteLine($"Estoque: {estoque}");
+            Console.WriteLine($"Frete: {frete}");
+            Console.WriteLine("=================================");
+        }
+    }
+}
